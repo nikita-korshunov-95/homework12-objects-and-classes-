f0baa497d9269871dc1d6eb4949fbec2bbbc411f
Nikita <nikita.korshunov.95@mail.ru>
2023-12-02 12:47:50 +0500

    Homework 12 (objects and classes)

--- src/Book.java
+++ src/Book.java
@@ -1,17 +1,17 @@
 public class Book {
     private String bookTitle;
-    private Author nameOfAuthor;
+    private Author author;
     private int publicationYear;
-    public Book (String bookTitle, Author nameOfAuthor, int publucationYear) {
+    public Book (String bookTitle, Author author, int publucationYear) {
         this.bookTitle = bookTitle;
-        this.nameOfAuthor = nameOfAuthor;
+        this.author = author;
         this.publicationYear = publucationYear;
     }
     public String getBookTitle () {
         return this.bookTitle;
     }
-    public Author getNameOfAuthor () {
-        return this.nameOfAuthor;
+    public Author getAuthor () {
+        return this.author;
     }
     public int getPublicationYear() {
         return this.publicationYear;
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Author johnTolkien = new Author ("John", " Tolkien");
-        System.out.println(johnTolkien.getNameAuthor() + johnTolkien.getSurnameAuthor());
 
         Author levTolstoy = new Author("Lev", " Tolstoy");
-        System.out.println(levTolstoy.getNameAuthor() + levTolstoy.getSurnameAuthor());
 
         Book lordOfTheRings = new Book("Lord of the Rings", johnTolkien, 1954);
         System.out.println(lordOfTheRings.getBookTitle());
-        System.out.println(lordOfTheRings.getNameOfAuthor());
+        System.out.println(lordOfTheRings.getAuthor().getNameAuthor() + "" + lordOfTheRings.getAuthor().getSurnameAuthor());
         System.out.println(lordOfTheRings.getPublicationYear());
 
         lordOfTheRings.setPublicationYear(1955);
@@ -16,7 +14,7 @@ public class Main {
 
         Book warAndPeace = new Book ("War and Peace", levTolstoy, 1869);
         System.out.println(warAndPeace.getBookTitle());
-        System.out.println(warAndPeace.getNameOfAuthor());
+        System.out.println(warAndPeace.getAuthor().getNameAuthor() + "" + warAndPeace.getAuthor().getSurnameAuthor());
         System.out.println(warAndPeace.getPublicationYear());
     }
 }
